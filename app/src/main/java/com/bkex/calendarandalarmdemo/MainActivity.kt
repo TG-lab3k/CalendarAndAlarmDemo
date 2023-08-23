b20903632eedf4147b27e515f9262ad1b2d4046b
brain <leigoti3@gmail.com>
2023-08-23 13:40:50 +0800

    dev alarm setting ui

--- app/src/main/java/com/bkex/calendarandalarmdemo/MainActivity.kt
+++ app/src/main/java/com/bkex/calendarandalarmdemo/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.bkex.calendarandalarmdemo
 
+import android.app.AlarmManager
+import android.app.AlarmManager.OnAlarmListener
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.AlarmManagerCompat
 import androidx.core.view.WindowCompat
 import com.bkex.calendarandalarmdemo.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +32,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
+                .setAnchorView(R.id.fab).setAction("Action", null).show()
+        }
+        binding.contentLayout.timeAddedBtn.setOnClickListener {
+            var time = binding.contentLayout.timeEditText.text.toString()
+            testAlarm(time)
         }
     }
 
@@ -44,5 +56,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun testAlarm(time: String) {
+        if (time.isNullOrBlank()) {
+            return
+        }
 
+        val timeDivider = time.split(":")
+        if (timeDivider.size != 2) {
+            return
+        }
+
+        val hour = timeDivider[0]
+        val minute = timeDivider[1]
+
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = System.currentTimeMillis()
+        calendar.set(Calendar.HOUR_OF_DAY, hour.toInt())
+        calendar.set(Calendar.MINUTE, minute.toInt())
+        calendar.set(Calendar.SECOND, 0)
+        calendar.set(Calendar.MILLISECOND, 0)
+
+        var alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            alarmManager.setExact(
+                AlarmManager.RTC_WAKEUP, calendar.timeInMillis, "", OnAlarmListener {
+                    Toast.makeText(applicationContext, "时间到：$time", Toast.LENGTH_LONG).show()
+                }, Handler()
+            )
+        }
+
+    }
 }
